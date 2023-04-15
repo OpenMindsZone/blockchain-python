e1057426395cfa551a4e7abc4735ba1982548742
RTavares - OpenMinds <openmindszone@protonmail.com>
2023-04-16 00:23:57 +0100

    tests - TD-2 - Update tests to the new structure.

--- project/test/test_blockchain.py
+++ project/test/test_blockchain.py
@@ -1,16 +1,19 @@
 """ Tests for Blockchain structures """
 import json
 
-from project.source.main import Blockchain, mine_block
+from project.source.main import Blockchain, chain_is_valid
 
 
 class TestBlockchain:
     def test_create_block(self):
         blockchain = Blockchain()
         assert isinstance(blockchain, Blockchain)
-
-        result = mine_block(blockchain)
-
         # Blockchain should be empty
-        assert result['index'] == 2
+        assert len(blockchain.chain) == 1
+
+        blockchain.add_block(data={'data': 'content_1'})
+        blockchain.add_block(data={'data': 'content_2'})
+
+        assert len(blockchain.chain) == 3
+        assert chain_is_valid(blockchain.chain) is True
 
